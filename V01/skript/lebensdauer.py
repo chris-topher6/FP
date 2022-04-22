93e442cd0d05206983a4d2152d47180444ce45d8
Christopher Breitfeld <christopher.breitfeld@tu-dortmund.de>
2022-04-22 16:47:57 +0200

    unsere daten

--- V01/skript/lebensdauer.py
+++ V01/skript/lebensdauer.py
@@ -8,7 +8,8 @@ from scipy.optimize import curve_fit
 def efunk(x, N0, l, U):
     return N0*np.exp(-l*x)+U 
 
-N = np.genfromtxt('data/ronja.dat', unpack=True)
+#N = np.genfromtxt('data/ronja.dat', unpack=True)
+N = np.genfromtxt('data/myonen.dat', unpack=True)
 K = np.linspace(1,len(N),len(N))
 
 #Kanäle in Zeit umrechnen
@@ -29,7 +30,8 @@ N_cut = np.delete(N, indexes)
 t_cut = np.delete(t, indexes)
 
 #Regression
-params, cov = curve_fit(efunk,  noms(t_cut),  N_cut)
+#params, cov = curve_fit(efunk,  noms(t_cut),  N_cut)
+params, cov = curve_fit(efunk,  noms(t[4:-57]),  N[4:-57])
 
 print("\nRegressionsparameter für die Lebensdauer sind")
 errors = np.sqrt(np.diag(cov))
@@ -37,9 +39,9 @@ for name, value, error in zip('NlU', params, errors):
     print(f'{name} = {value:.8f} ± {error:.8f}')
 
 #Plot
-x=noms(np.linspace(np.min(t), np.max(t)))
+x=noms(np.linspace(np.min(t[4:-57]), np.max(t[4:-57])))
 plt.errorbar(noms(t),     N,     xerr=stds(t),     yerr=np.sqrt(N),     color='red', ecolor='grey',  markersize=3.5, elinewidth=0.5, fmt='.', label="entfernte Daten")
-plt.errorbar(noms(t_cut), N_cut, xerr=stds(t_cut), yerr=np.sqrt(N_cut), color='navy', ecolor='grey', markersize=3.5, elinewidth=0.5, fmt='.', label="Daten")
+plt.errorbar(noms(t[4:-57]), N[4:-57], xerr=stds(t[4:-57]), yerr=np.sqrt(N[4:-57]), color='navy', ecolor='grey', markersize=3.5, elinewidth=0.5, fmt='.', label="Daten")
 plt.plot(x, efunk(x, params[0], params[1], params[2]), color='orangered', label="Fit")
 plt.xlabel(r"$t[\mu s]$")
 plt.ylabel(r"$N[1/s]$")
@@ -51,4 +53,4 @@ plt.savefig('build/lebensdauer.pdf')
 #Berechnung der mittleren Lebensdauer
 lam=ufloat(params[1], errors[1])
 tau=1/lam
-print(f"\ndie mittlere Lebensdauer beträgt tau=({tau})us")
+print(f"\ndie mittlere Lebensdauer beträgt tau=({tau:.4})us")
