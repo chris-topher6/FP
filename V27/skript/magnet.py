0af3ccb61d045f3d90e93cbaa660359418021640
Christopher Breitfeld <christopher.breitfeld@tu-dortmund.de>
2022-05-23 19:25:37 +0200

    auswertung funktioniert soweit

--- V27/skript/magnet.py
+++ V27/skript/magnet.py
@@ -1,23 +1,29 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+def poly3(a,b,c,d,x):
+    return a*x**3+b*x**2+c*x+d
+
 I, B = np.genfromtxt('data/magnet.dat', unpack=True)
 
-params, covariance_matrix = np.polyfit(I, B, deg=1, cov=True)
+params, covariance_matrix = np.polyfit(I, B, deg=3, cov=True)
 uncertainties = np.sqrt(np.diag(covariance_matrix))
 
 #Ausgeben der Parameter
-print("\nRegressionsparameter für Platau für justage 15")
+print("\nRegressionsparameter für vollständige Regression")
 errors = np.sqrt(np.diag(covariance_matrix))
-for name, value, error in zip('ab', params, errors):
+for name, value, error in zip('abcd', params, errors):
     print(f'{name} = {value:.8f} ± {error:.8f}')
 
 #Plot
 x = np.linspace(np.min(I), np.max(I))
-plt.plot(x,params[0]*x+params[1], color="darkgreen", label="Regression")
+x2= np.linspace(np.min(I), 5)
+plt.plot(x, poly3(params[0],params[1],params[2],params[3],x), color="darkgreen", label="Regression")
 plt.plot(I,B, "x", color="darkblue", label="Daten")
 plt.xlabel(r"$I [A]$")
 plt.ylabel(r"$B [mT]$")
 plt.legend()
 plt.tight_layout()
 plt.savefig('build/magnet.pdf')
+
+print(f"B(4.5A)={poly3(params[0],params[1],params[2],params[3],4.5)}")
