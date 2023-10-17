4db247fca14abc32fbb97ebf00328d66e47afb7c
Christopher Breitfeld <christopher.breitfeld@tu-dortmund.de>
2023-10-17 21:14:00 +0200

    ob das so richtig ist...

---
+++ Master/V61/script/stability_theory.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def stability(r1, r2, L):
+    g1 = 1-L/r1
+    g2 = 1-L/r2 
+    return g1*g2 
+
+r = [1, 1.4, 10**9]
+L = np.linspace(0,1.5,100)
+
+plt.plot(L, stability(r[0], r[1], L), label='c 1000, c 1400')
+plt.plot(L, stability(r[0], r[2], L), label='c 1000, f')
+plt.plot(L, stability(r[1], r[1], L), label='c 1400, c 1400')
+plt.plot(L, stability(r[1], r[2], L), label='c 1400, f')
+plt.grid()
+plt.legend()
+plt.tight_layout()
+#plt.ylim(0, 1) 
+plt.savefig("build/stability_theory.pdf")
