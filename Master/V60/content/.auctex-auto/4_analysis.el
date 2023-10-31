d2338d984beba3d1f27a69fc6dd3d103f81997fe
Christopher Breitfeld <christopher.breitfeld@tu-dortmund.de>
2023-10-31 14:25:49 +0100

    erster commit

---
+++ Master/V60/content/.auctex-auto/4_analysis.el
@@ -0,0 +1,21 @@
+(TeX-add-style-hook
+ "4_analysis"
+ (lambda ()
+   (LaTeX-add-labels
+    "fig:distance-intensity"
+    "tab:distance-intensity"
+    "eqn:TEM00-Fit"
+    "fig:TEM-Messung1"
+    "eqn:TEM10-Fit"
+    "fig:TEM-Messung2"
+    "eqn:intensity-angle"
+    "fig:polarisation"
+    "tab:intensity-angles"
+    "eqn:modetheo"
+    "eqn:modetheomultiple"
+    "tab:multimode"
+    "tab:deltaf"
+    "eqn:wavelength"
+    "tab:opticalgr"))
+ :latex)
+
