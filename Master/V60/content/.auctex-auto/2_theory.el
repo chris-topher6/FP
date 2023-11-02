0543cd70f81d8da74652731bf78bc97008b85621
Henry Krämerkämper <henry.kraemerkaemper@tu-dortmund.de>
2023-11-02 23:48:59 +0100

    Sehr kleiner Anfang (hat aber eine gute Technik)

--- Master/V60/content/.auctex-auto/2_theory.el
+++ Master/V60/content/.auctex-auto/2_theory.el
@@ -3,11 +3,6 @@
  (lambda ()
    (LaTeX-add-labels
     "sec:theory"
-    "fig:emission"
-    "fig:level"
-    "eqn:stability"
-    "fig:resonator"
-    "fig:TEM"
-    "eqn:TEM"))
+    "sec:diodelaser"))
  :latex)
 
