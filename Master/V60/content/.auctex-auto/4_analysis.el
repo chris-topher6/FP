1357adabec318890a400b50375fd1597fb2d07d3
Henry Krämerkämper <henry.kraemerkaemper@tu-dortmund.de>
2023-11-07 18:18:58 +0100

    Ein ganz kleiner Rechtschreibfehler

--- Master/V60/content/.auctex-auto/4_analysis.el
+++ Master/V60/content/.auctex-auto/4_analysis.el
@@ -2,20 +2,12 @@
  "4_analysis"
  (lambda ()
    (LaTeX-add-labels
-    "fig:distance-intensity"
-    "tab:distance-intensity"
-    "eqn:TEM00-Fit"
-    "fig:TEM-Messung1"
-    "eqn:TEM10-Fit"
-    "fig:TEM-Messung2"
-    "eqn:intensity-angle"
-    "fig:polarisation"
-    "tab:intensity-angles"
-    "eqn:modetheo"
-    "eqn:modetheomultiple"
-    "tab:multimode"
-    "tab:deltaf"
-    "eqn:wavelength"
-    "tab:opticalgr"))
+    "fig:a"
+    "fig:b"
+    "fig:threshold"
+    "fig:fluorescence"
+    "fig:spectrum_1"
+    "fig:spectrum_2"
+    "fig:spectrum"))
  :latex)
 
