10d7e570c47175748ad972207680c011417b14d6
Christopher Breitfeld <christopher.breitfeld@tu-dortmund.de>
2023-11-09 02:58:03 +0100

    erster commit

---
+++ Master/V18/content/.auctex-auto/2_theory.el
@@ -0,0 +1,18 @@
+(TeX-add-style-hook
+ "2_theory"
+ (lambda ()
+   (LaTeX-add-labels
+    "sec:theory"
+    "sec:diodelaser"
+    "fig:diode"
+    "fig:laser-system"
+    "sec:tuning"
+    "fig:diode-frequency"
+    "sec:mediumgain"
+    "sec:internalcavitgain"
+    "sec:opticalgratinggain"
+    "sec:externalcavitygain"
+    "sec:modehops"
+    "fig:hophop"))
+ :latex)
+
---
+++ Master/V18/content/.auctex-auto/3_procedure.el
@@ -0,0 +1,13 @@
+(TeX-add-style-hook
+ "3_procedure"
+ (lambda ()
+   (LaTeX-add-labels
+    "sec:procedure"
+    "sec:lasersetup"
+    "fig:laserhead"
+    "sec:measurements"
+    "sec:threshold"
+    "sec:rubidium"
+    "fig:structure"))
+ :latex)
+
