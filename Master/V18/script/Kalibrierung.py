bc8b731372732841c418ec3b2a9980a81f8f1e43
Henry Krämerkämper <henry.kraemerkaemper@tu-dortmund.de>
2023-11-22 13:43:33 +0100

    Daten + ein Plot

---
+++ Master/V18/script/Kalibrierung.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+SKIP_ANFANG = 12
+SKIP_ENDE = 14
+
+europium = pd.read_csv("./data/Eu1.Spe", skiprows=SKIP_ANFANG, header=None)
+europium = europium.iloc[:-SKIP_ENDE]  # Entferne den Rotz am Ende
+europium.columns = ["data"]
+europium["index"] = europium.index
+
+# Sicherstellen, dass die Spalte "data" numerische Werte enthält
+europium["data"] = pd.to_numeric(europium["data"], errors="coerce")
+
+plt.figure(figsize=(21, 9))
+
+plt.bar(europium["index"], europium["data"], linewidth=2)
+
+plt.xticks(np.linspace(0, 8191, 10))
+plt.yticks(np.linspace(europium["data"].min(), europium["data"].max(), 10))
+
+plt.ylim(europium["data"].min() - 30)
+
+plt.xlabel(r"Kanäle")
+plt.ylabel(r"Signale")
+
+plt.grid(True, linewidth=0.1)
+
+plt.savefig("./build/Europium-Test.pdf")
