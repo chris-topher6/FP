47f00a38843ac20b17e4d47bb9c04b83399260c3
Henry Krämerkämper <henry.kraemerkaemper@tu-dortmund.de>
2023-11-24 23:17:17 +0100

    Noch mehr Kleinkrams

--- Master/V18/script/Kalibrierung.py
+++ Master/V18/script/Kalibrierung.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.signal import find_peaks
 
+MAKE_PLOT = True
+
 
 # Einlesen der Kalibrationsmessung
 SKIP_ANFANG = 12
@@ -18,22 +20,33 @@ europium["index"] = europium.index
 # Sicherstellen, dass die Spalte "data" numerische Werte enthält
 europium["data"] = pd.to_numeric(europium["data"], errors="coerce")
 
-peaks, _ = find_peaks(europium["data"], height=20, prominence=20, distance=30)
+peaks, _ = find_peaks(europium["data"], height=20, prominence=20, distance=10)
+peaks = pd.Series(peaks)
 
-# Plot der Kalibrationsmessung
-plt.figure(figsize=(21, 9))
+# Peaks die eher dem Untergrundrauschen zuzuordnen sind entfernen
+peaks = peaks.drop([0,1,2,3,6,7,8,9,10,12,13,14,15,16])
 
-plt.bar(europium["index"], europium["data"], linewidth=2, width=1.1)
-plt.plot(peaks, europium["data"][peaks], "x", color="orange")
+print(len(peaks))
+print(peaks.info())
 
-plt.xticks(np.linspace(0, 8191, 10))
-plt.yticks(np.linspace(europium["data"].min(), europium["data"].max(), 10))
+print(europium.info())
 
-plt.ylim(europium["data"].min() - 30)
 
-plt.xlabel(r"Kanäle")
-plt.ylabel(r"Signale")
+if MAKE_PLOT == True:
+    # Plot der Kalibrationsmessung
+    plt.figure(figsize=(21, 9))
 
-plt.grid(True, linewidth=0.1)
+    plt.bar(europium["index"], europium["data"], linewidth=2, width=1.1)
+    plt.plot(peaks, europium["data"][peaks], "x", color="orange")
 
-plt.savefig("./build/Europium-Test.pdf")
+    plt.xticks(np.linspace(0, 8191, 10))
+    plt.yticks(np.linspace(europium["data"].min(), europium["data"].max(), 10))
+
+    plt.ylim(europium["data"].min() - 30)
+
+    plt.xlabel(r"Kanäle")
+    plt.ylabel(r"Signale")
+
+    plt.grid(True, linewidth=0.1)
+
+    plt.savefig("./build/Europium-Test.pdf")
