af3bbf2eab104e5b8ac74366d86a262af21bd37d
Christopher Breitfeld <christopher.breitfeld@tu-dortmund.de>
2023-12-12 12:56:00 +0100

    erster commit

---
+++ Master/V64/content/.auctex-auto/2_theory.el
@@ -0,0 +1,13 @@
+(TeX-add-style-hook
+ "2_theory"
+ (lambda ()
+   (LaTeX-add-labels
+    "sec:theory"
+    "fig:emission"
+    "fig:level"
+    "eqn:stability"
+    "fig:resonator"
+    "fig:TEM"
+    "eqn:TEM"))
+ :latex)
+
---
+++ Master/V64/content/.auctex-auto/3_procedure.el
@@ -0,0 +1,8 @@
+(TeX-add-style-hook
+ "3_procedure"
+ (lambda ()
+   (LaTeX-add-labels
+    "sec:procedure"
+    "fig:brewster"))
+ :latex)
+
