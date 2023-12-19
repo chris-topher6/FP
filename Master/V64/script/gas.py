e1de2e8edd05ecff6e40f4e1892ec1cce394debf
Christopher Breitfeld <christopher.breitfeld@tu-dortmund.de>
2023-12-19 10:46:09 +0100

    erste skripte

---
+++ Master/V64/script/gas.py
@@ -0,0 +1,79 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.constants as const
+import scipy.optimize as op
+import uncertainties.unumpy as unp
+from uncertainties import ufloat
+from uncertainties.unumpy import (nominal_values as noms,
+                                  std_devs as stds)
+
+lamda = 632.99e-9 # wavelength of the laser (meter)
+
+# Refractive index of air
+
+p, m1, m2, m3, m4, m5 = np.genfromtxt("data/gas.dat", unpack = True)
+
+T_0 = 273.15 + 22.2 # K Raumtemperatur
+L = ufloat(0.1, 0.1e-3)
+
+def n_air_exp(M):
+    return M*lamda/(L) + 1
+
+M = unp.uarray(np.mean([m1, m2, m3, m4, m5], axis = 0), np.std([m1, m2, m3, m4, m5], axis = 0))
+
+n = n_air_exp(M)
+print("---------------------------------------------------")
+print("Brechungsindices Luft:")
+for i in range(len(n)):
+    print(f"{n[i]:.7f}")
+print("---------------------------------------------------")
+
+def n_air_theo(p, A, b):
+    return (3/2) * A * p/(const.R *T_0) + b
+
+params2, pcov2 = op.curve_fit(n_air_theo, p, noms(n))   
+err2 = np.sqrt(np.diag(pcov2))
+
+n_air_exp = 3/2 * ufloat(params2[0], err2[0])*(1013)/(const.R *(273.15 + 22.5)) + ufloat(params2[1], err2[1])
+
+print("--------------------------------------------------")
+print("Fit: Refractive Index of Air:")
+print(f"A = {params2[0]:.4e} +- {err2[0]:.4e}")
+print(f"b = {params2[1]:.8f} +- {err2[1]:.8f}")
+print(f"Experimental Value: n = {n_air_exp}")
+print("--------------------------------------------------")
+
+x = np.linspace(0, 1000, 100)
+
+fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, gridspec_kw={'height_ratios': [3, 1]})
+
+# Daten und Fit im Hauptplot darstellen
+ax1.errorbar(p, noms(n), yerr=stds(n), fmt='.', markersize=5, color='black', ecolor='dimgray', elinewidth=2, capsize=2, label="Data")
+ax1.plot(x, n_air_theo(x, *params2), color='dodgerblue', linestyle='-', linewidth=2, label="Fit")
+
+# Beschriftungen und Grenzen für den Hauptplot
+ax1.set_ylabel(r"$n$", fontsize=12)
+ax1.legend(fontsize=10)
+ax1.grid(True, linestyle='--', alpha=0.7)
+
+# Berechnung der Residuen für den Pull-Plot
+pulls = (noms(n) - n_air_theo(p, *params2)) / stds(n)
+ax2.plot(p, pulls, 'o', markersize=5, color='red')
+ax2.axhline(0, color='black', linestyle='--', linewidth=1)  # Referenzlinie bei 0
+ax2.set_ylim(-1.5, 1.5)
+
+# Beschriftungen und Grenzen für den Pull-Plot
+ax2.set_ylabel("Pulls/$\sigma$")
+ax2.set_xlabel(r"$p\ $mb", fontsize=12)
+ax2.grid(True, linestyle='--', alpha=0.7)
+
+# Layout anpassen
+plt.tight_layout()
+
+# Hintergrundfarbe setzen
+fig.patch.set_facecolor('whitesmoke')
+
+# Speichern des Plots in einer PDF-Datei
+plt.savefig("build/n_air.pdf", bbox_inches='tight')
+# Schließen des Plots, um Speicher freizugeben
+plt.close()
---
+++ Master/V64/script/glas.py
@@ -0,0 +1,106 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.constants as const
+import scipy.optimize as op
+import uncertainties.unumpy as unp
+from uncertainties import ufloat
+from uncertainties.unumpy import (nominal_values as noms,
+                                  std_devs as stds)
+
+# Wavelength of the laser in meters
+lamda = 632.99e-9 
+
+# Determination of the refractive index of glass
+
+# Reading data from the file and unpacking it into variables
+theta, M1, M2, M3, M4, M5, M6, M7, M8, M9 = np.genfromtxt("data/glas.txt", unpack = True)
+
+# Converting theta from degrees to radians
+theta = theta*np.pi/180
+theta = theta[1:]
+
+# Liste, um die transformierten Daten zu speichern
+transformed_M = []
+
+# Funktion, um die Transformation durchzuführen
+def transform_data(data):
+    differences = -np.diff(data)  # Differenzen berechnen und umkehren
+    cumulative_sum = np.cumsum(differences, axis=0)
+#    cumulative_sum = np.insert(cumulative_sum, 0, 0)  # 0 am Anfang einfügen
+    return cumulative_sum
+
+# Transformation für jede Messreihe durchführen
+for data in [M1, M2, M3, M4, M5, M6, M7, M8, M9]:
+    transformed_M.append(transform_data(data))
+
+# Umwandeln der Liste in ein NumPy-Array für eine einfachere Handhabung
+transformed_M = np.array(transformed_M)
+
+# Berechnung des Mittelwerts für jede Spalte in der Matrix 'transformed_M'
+M_mean = np.mean(transformed_M, axis=0)
+M_std = np.std(transformed_M, axis=0)
+
+# Printing the mean and standard deviation of the measurements
+print("--------------------------------------------------")
+for i in range(len(M_mean)):
+    print(f"{M_mean[i]:.4f} +- {M_std[i]:.4f}")
+print("--------------------------------------------------")
+
+# Thickness of the glass pane in meters
+T = 1e-3 
+# Initial angle in radians
+alpha_0 = 10*np.pi/180
+
+# Function to calculate the number of maxima
+def Maxima(theta, n):
+    return 2*T/lamda * (n-1)/n *alpha_0*theta
+
+# Curve fitting to determine the refractive index
+params1, pcov1 = op.curve_fit(Maxima, theta, M_mean, sigma = M_std)
+err1 = np.sqrt(np.diag(pcov1))
+
+# Calculating the relative deviation from a known value
+d = np.abs(params1[0]-1.515)/1.515
+
+# Printing the results
+print("--------------------------------------------------")
+print("Refractive Index Glass:")
+print(f"n = {params1[0]:.4f} +- {err1[0]:.4f}")
+print(f"Relative deviation: {d*100}%")
+print("--------------------------------------------------")
+
+# Generating points for the fit curve
+x = np.linspace(0, 10, 100)
+
+# Creating a plot with two subplots
+fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, gridspec_kw={'height_ratios': [3, 1]})
+
+# Plotting the data and Fit
+ax1.errorbar(theta*180/np.pi, M_mean, yerr=M_std, fmt='o', markersize=5, color='black', ecolor='dimgray', elinewidth=2, capsize=2, label="Data")
+ax1.plot(x, Maxima(x*np.pi/180, *params1), color='dodgerblue', linestyle='-', linewidth=2, label="Regression")
+
+# Setting labels and limits for the main plot
+ax1.set_ylabel(r"$M$", fontsize=12)
+ax1.legend(fontsize=10)
+ax1.grid(True, linestyle='--', alpha=0.7)
+
+# Calculating residuals for the pull plot
+pulls = (M_mean - Maxima(theta, *params1)) / M_std
+ax2.plot(theta*180/np.pi, pulls, 'o', markersize=5, color='red')
+ax2.axhline(0, color='black', linestyle='--', linewidth=1)  # Zero line for reference
+ax2.set_ylim(-1, 1)
+
+# Setting labels and limits for the pull plot
+ax2.set_ylabel("Pulls/$\sigma$")
+ax2.set_xlabel(r"$\theta / °$", fontsize=12)
+ax2.grid(True, linestyle='--', alpha=0.7)
+
+# Adjust layout
+plt.tight_layout()
+
+# Set background color
+fig.patch.set_facecolor('whitesmoke')
+
+# Saving the plot to a PDF file
+plt.savefig("build/n_glass.pdf", bbox_inches='tight')
+plt.close()
