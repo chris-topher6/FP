5c8c6a75ec0c3f087d2e3b7ea3cf575456878047
Henry Krämerkämper <henry.kraemerkaemper@tu-dortmund.de>
2023-12-31 16:54:26 +0100

    Neu geschrieben, noch mit Fehler

--- Master/V18/script/Linienbreite.py
+++ Master/V18/script/Linienbreite.py
@@ -8,6 +8,7 @@ import iminuit
 from iminuit import Minuit
 from iminuit.cost import ExtendedBinnedNLL
 from scipy.stats import norm
+from typing import Tuple
 
 matplotlib.rcParams.update({"font.size": 18})
 
@@ -111,39 +112,86 @@ def scaled_gauss_cdf(x, s, mu, sigma):
     die an die Signale angepasst werden soll.
 
     Parameter:
+
     x: array-like, bin edges; muss len(n) + 1 sein (n: Anzahl bins)
+
     s: Skalierungsparameter der Funktion
+
     mu: Erwartungswert der Normalverteilung
+
     sigma: Standardabweichung der Normalverteilung
     """
     return s * norm(mu, sigma).cdf(x)
 
 
-for i in range(len(peaks)):
-    # Bereich um den jeweiligen Peak herum ausschneiden
-    maske = (europium["index"] >= peaks["peaks"][i] - 50) & (
-        europium["index"] <= peaks["peaks"][i] + 50
+def fitmaker_2000(
+    abstand_links: int,
+    abstand_rechts: int,
+    peak_idx: int,
+    startwert_schätzen: bool = True,
+    peak_width: float = 0,
+    peak_position: float = 0,
+    peak_height: float = 0,
+    limit_s: Tuple = (0, None),
+    limit_mu: Tuple = (0, None),
+    limit_sigma: Tuple = (0, None),
+) -> None:
+    """
+    Funktion die einen Fit einer skalierbaren Gauß-Funktion vornimmt. Es kann
+    eine Schätzung der Startparameter angegeben werden; alternativ wird eine rudimentäre Schätzung durchgeführt.
+
+    Parameter:
+
+    abstand_links: int, gibt an, wo die linke Grenze des zu fittenden Bereichs sein soll
+
+    abstand_rechts: int, gibt an, wo die rechte Grenze des zu fittenden Bereichs sein soll
+
+    peak_idx: int, gibt an, welcher der 13 Peaks gefittet werden soll
+
+    startwert_schätzen: bool, wechselt zwischen automatisch oder manuell geschätzten Startwerten
+
+    peak_width: float, optionaler Schätzwert für sigma, wird nur verwendet wenn startwert_schätzen auf False gesetzt wird
+
+    peak_position: float, optional, wird nur verwendet wenn startwert_schätzen auf False gesetzt wird
+
+    peak_height: float, optionaler Schätzwert für s, wird nur verwendet wenn startwert_schätzen auf False gesetzt wird
+
+    limit_s: Tuple, optionaler (oberer und unterer) Grenzwert s
+
+    limit_mu: Tuple, optionaler (oberer und unterer) Grenzwert mu
+
+    limit_sigma: Tuple, optionaler (oberer und unterer) Grenzwert sigma
+    """
+
+    # Bereich um den Peak herum ausschneiden
+    maske = (europium["index"] >= peaks["peaks"][peak_idx] - abstand_links) & (
+        europium["index"] <= peaks["peaks"][peak_idx] + abstand_rechts
     )
     europium_cut = europium[maske]
 
-    # Erstelle Bin-Kanten für den abgeschnittenen Datensatz
-    cut_bin_edges = np.arange(
-        europium_cut["index"].min(), europium_cut["index"].max() + 2
-    )
+    # Kanten der Bins für den abgeschnittenen Datensatz
+    cut_bin_edges = np.arange(europium_cut["index"].min(), europium["index"].max() + 2)
+    print(len(cut_bin_edges))
+    print(len(europium["daten"]))
+
+    # TODO Problem der falschen Länge der bin Kanten lösen, in der Schleife hats noch funktioniert
 
     cost = ExtendedBinnedNLL(europium_cut["daten"], cut_bin_edges, scaled_gauss_cdf)
 
-    # Schätze s über die maximale Höhe des Peaks
-    peak_height = europium_cut["daten"].max()
+    if startwert_schätzen == True:
+        # Schätze s über die maximale Höhe des Peaks
+        peak_height = europium_cut["daten"].max()
 
-    # Als Startwert für mu: Position des Peaks
-    peak_position = peaks["peaks"][i]
+        # Als Startwert für mu: Position des Peaks
+        peak_position = peaks["peaks"][peak_idx]
 
-    # Schätze die Breite des Peaks (sigma)
-    peak_width = (europium_cut["index"].max() - europium_cut["index"].min()) * 0.1
+        # Schätze die Breite des Peaks (sigma)
+        peak_width = (europium_cut["index"].max() - europium_cut["index"].min()) * 0.1
 
     m = Minuit(cost, s=peak_height, mu=peak_position, sigma=peak_width)
-    m.limits["s", "mu", "sigma"] = (0, 10000)
+    m.limits["s"] = limit_s
+    m.limits["mu"] = limit_mu
+    m.limits["sigma"] = limit_sigma
     m.migrad()
     m.hesse()
 
@@ -157,7 +205,7 @@ for i in range(len(peaks)):
         linewidth=2,
         width=1.1,
         color="blue",
-        label=r"$^{152}\mathrm{Eu}$" + f"-Peak {i+1}",
+        label=r"$^{152}\mathrm{Eu}$" + f"-Peak {peak_idx+1}",
     )
     plt.stairs(
         np.diff(scaled_gauss_cdf(cut_bin_edges, *m.values)),
@@ -171,5 +219,9 @@ for i in range(len(peaks)):
     plt.xlabel(r"$\mathrm{Channels}$")
     plt.ylabel(r"$\mathrm{Energy}/\mathrm{keV}$")
     plt.tight_layout()
-    plt.savefig(f"./build/Europium-Fit-Peak{i}.pdf")
+    plt.savefig(f"./build/Europium-Fit-Peak{peak_idx}.pdf")
     plt.clf()
+
+
+for i in range(len(peaks)):
+    fitmaker_2000(20, 20, i)
