a9a75e9de8955350f56a36f5962b3a9b57c78743
Christopher Breitfeld <christopher.breitfeld@tu-dortmund.de>
2024-01-18 15:11:08 +0100

    Theorie geschrieben

---
+++ Master/V47/content/.auctex-auto/2_theory.el
@@ -0,0 +1,17 @@
+(TeX-add-style-hook
+ "2_theory"
+ (lambda ()
+   (LaTeX-add-labels
+    "sec:theory"
+    "sec:interference"
+    "eqn:degree"
+    "fig:PBSC"
+    "eqn:contrast"
+    "eqn:intensity"
+    "eqn:contrast2"
+    "eqn:M"
+    "eqn:delta1"
+    "eqn:delta"
+    "eqn:LLL"))
+ :latex)
+
