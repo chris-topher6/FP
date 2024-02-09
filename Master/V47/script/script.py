e5cb279f2d970ba3ed461a89a56e94912b4ede81
Henry Krämerkämper <henry.kraemerkaemper@tu-dortmund.de>
2024-02-09 22:18:41 +0100

    Einiges gefixt

--- Master/V47/script/script.py
+++ Master/V47/script/script.py
@@ -4,9 +4,12 @@ import matplotlib.pyplot as plt
 from scipy.interpolate import CubicSpline
 from uncertainties import ufloat
 
+# Pandas dazu zwingen, alle Spalten anzuzeigen
+pd.set_option("display.max_columns", None)
+
 # Konstanten definieren
-MOLARE_MASSE_CU = 63.546  # M [g/mol]
-PROBENMASSE = 342  # m [g]
+MOLARE_MASSE_CU = 0.06355  # M [kg/mol]
+PROBENMASSE = 0.342  # m [kg]
 PROBENDICHTE = 8.96  # rho [g/cm^3]
 KOMPRESSIONSMODUL = 137.8  # kappa [GPa]
 
@@ -36,6 +39,7 @@ def temp_berechnung(R):
     return T
 
 
+# Umrechnung in Kelvin
 messung["T_G"] = temp_berechnung(messung["R_G"]) + 273.15
 messung["T_P"] = temp_berechnung(messung["R_P"]) + 273.15
 messung["T"] = (messung["T_G"] + messung["T_P"]) / 2
@@ -49,19 +53,24 @@ messung["DeltaT"] = messung["T"].diff()
 messung["DeltaT"].fillna(1, inplace=True)  # ACHTUNG die 1 ist nur als Platzhalter da
 # t [min], R_G [Ohm], R_P [Ohm], I [mA], U [V], T_G [K], T_P [K], T[K] Deltat [min], DeltaT [K]
 
+# Damit am Ende [C_p] = [J/(mol*K)] rauskommt: I in [A], Deltat in [s] umrechnen:
+messung["Deltat"] = messung["Deltat"] * 60
+messung["I_err"] = messung["I_err"] / 1000
+# t [min], R_G [Ohm], R_P [Ohm], I [A], U [V], T_G [K], T_P [K], T[K] Deltat [s], DeltaT [K]
+
 
 def Cp_berechnen(U, I, Deltat, DeltaT, M, m):
     """
     Funktion die die Wärmekapazität bei konstantem Druck aus den folgenden Parametern berechnet:
     Args:
-    U: Spannung
-    I: Strom
-    Deltat: Zeitabstand
-    DeltaT: Temperaturänderung
-    M: molare Masse der Probe
-    m: Masse der Probe
+    U: Spannung [V]
+    I: Strom [A]
+    Deltat: Zeitabstand, [s]
+    DeltaT: Temperaturänderung, [K]
+    M: molare Masse der Probe, [kg/mol]
+    m: Masse der Probe, [kg]
     Returns:
-    C_p: Wärmekapazität bei konstantem Druck [J * mol /]
+    C_p: Wärmekapazität bei konstantem Druck [J / (mol*K)]
     """
     C_p = (U * I * Deltat * M) / (DeltaT * m)
     return C_p
@@ -117,7 +126,7 @@ def CV_berechnen(C_p, alpha, kappa, V0, T):
     """
     Funktion zur Berechnung der Wärmekapazität bei konstantem Volumen aus der Wärmekapazität bei konstantem Druck.
     Args:
-    C_p: isobare Wärmekapazität
+    C_p: isobare Wärmekapazität, [J/(mol * K)]
     alpha: Koeffizient der thermischen Ausdehnung
     kappa: Kompressionsmodul des Kupfers
     V0: Volumen??
@@ -173,7 +182,7 @@ werte_theta["C_V"] = pd.Series(werte_theta_arsch.values.flatten())
 werte_theta = werte_theta.sort_values(by="C_V")
 
 # C_V von J/(mol K) in J/(g K) umrechnen; hierfür muss mit der molaren Masse multipliziert werden
-werte_theta["C_V"] = werte_theta["C_V"] * MOLARE_MASSE_CU
+# werte_theta["C_V"] = werte_theta["C_V"] * MOLARE_MASSE_CU
 # # in J/(kg K)
 # werte_theta["C_V"] = werte_theta["C_V"] * 1000
 
